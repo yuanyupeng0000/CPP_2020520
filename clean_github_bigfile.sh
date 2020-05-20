1ee4502f8d0bce9c2ac4e3bf5639e9b6d873650b
yuanyupeng0000 <yuanyupeng-1989@163.com>
2020-05-20 15:48:10 +0800

    first add

--- clean_github_bigfile.sh
+++ clean_github_bigfile.sh
@@ -3,4 +3,5 @@ sudo git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch 
 sudo git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch *.264' --prune-empty --tag-name-filter cat -- --all
 sudo git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch *.mp4' --prune-empty --tag-name-filter cat -- --all
 sudo git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch *.tgz' --prune-empty --tag-name-filter cat -- --all
+sudo git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch *.wk' --prune-empty --tag-name-filter cat -- --all
 sudo git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch *.tar.gz' --prune-empty --tag-name-filter cat -- --all
